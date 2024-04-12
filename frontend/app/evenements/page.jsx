5c161b55f470e6d49b6e27028d97d4ea9193a392
Alexis B <alexisbouffard1007@gmail.com>
2024-04-12 15:47:15 +0200

    Modification titre page

---
+++ frontend/app/evenements/page.jsx
@@ -0,0 +1,9 @@
+export default function Evenements () {
+  return (
+    <section>
+      <h1 className='text-4xl text-center my-8'>Calendrier</h1>
+      <p>Salut à tous</p>
+    </section>
+      
+  )
+}
