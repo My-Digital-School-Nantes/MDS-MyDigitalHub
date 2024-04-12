c107a04dc892b7e0411f6fa7402a04f04821d671
julienNtb <julien.nottebaert1@gmail.com>
2024-04-12 16:22:26 +0200

    ajout de card

--- frontend/components/evenements/EventList.jsx
+++ frontend/components/evenements/EventList.jsx
@@ -1,5 +1,25 @@
+import React from 'react'
+import { Card, CardHeader, CardBody, Image } from '@nextui-org/react'
+
 export function EventList () {
   return (
-    <div>eventList</div>
+    <section className='container'>
+      <h1>Liste des évènements</h1>
+      <Card className='py-4'>
+        <CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
+          <p className='text-tiny uppercase font-bold'>Daily Mix</p>
+          <small className='text-default-500'>12 Tracks</small>
+          <h4 className='font-bold text-large'>Frontend Radio</h4>
+        </CardHeader>
+        <CardBody className='overflow-visible py-2'>
+          <Image
+            alt='Card background'
+            className='object-cover rounded-xl'
+            src='/images/hero-card-complete.jpeg'
+            width={270}
+          />
+        </CardBody>
+      </Card>
+    </section>
   )
 }
