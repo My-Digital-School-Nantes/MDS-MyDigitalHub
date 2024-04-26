8fbae89dc457c4ac06b9f5813d932414628ddf3f
Romain Garganigo <romain.garganigo@sensorfactory.eu>
2024-04-26 15:04:00 +0200

    fix nextauth

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -41,8 +41,7 @@ const handler = NextAuth({
   pages: {
     signIn: '/auth/signin',
     signOut: '/auth/signout',
-    error: '/auth/error', // Error code passed in query string as ?error=
-    job: '/joboffers'
+    error: '/auth/error'// Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // (used for check email message)
     // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   }
