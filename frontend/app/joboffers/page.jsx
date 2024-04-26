4a2cba710e5c4c8603d9bb04ac09dc05c3f4d505
Phaerya <joaquim.morais-faria@outlook.com>
2024-04-26 14:59:57 +0200

    -#2 Modification to add graphql instead of fetch Change front to suit romain's work

--- frontend/app/joboffers/page.jsx
+++ frontend/app/joboffers/page.jsx
@@ -1,5 +1,3 @@
-'use client'
-import React, { useState, useEffect } from 'react'
 import {
   Card, CardHeader, CardBody, CardFooter, Divider, Image,
   Dropdown,
@@ -11,21 +9,24 @@ import {
   , Input
   , ScrollShadow
 } from '@nextui-org/react'
-
 import { LuSearch } from 'react-icons/lu'
+import { GET_OFFERS } from '@/graphql/queries/queries'
+import client from '@/graphql/apolloClient'
 
-export default function JobOffers () {
-  const [jobOffers, setJobOffers] = useState([])
+export const getData = async () => {
+  try {
+    const response = await client.query({
+      query: GET_OFFERS
+    })
+    return response.data.offers.data
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-  useEffect(() => {
-    fetch('http://localhost:1337/api/offers')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Data fetched:', data)
-        setJobOffers(data.data)
-      })
-      .catch(error => console.error('Error fetching job offers:', error))
-  }, [])
+export default async function JobOffers () {
+  const data = await getData()
+  console.log('data', data)
 
   const colors = [
     'default', 'primary', 'secondary', 'success', 'warning', 'danger', 'foreground'
@@ -43,34 +44,11 @@ export default function JobOffers () {
     <>
       <h1 className='text-4xl text-center my-8'>MDS Job Offers</h1>
       <div>
-        <Input
-          label='Search'
-          isClearable
-          radius='lg'
-          classNames={{
-            label: 'text-black/50 dark:text-white/90',
-            input: [
-              'bg-transparent', 'text-black/90 dark:text-white/90',
-              'placeholder:text-default-700/50 dark:placeholder:text-white/60'
-            ],
-            innerWrapper: 'bg-transparent',
-            inputWrapper: [
-              'shadow-xl', 'bg-default-200/50', 'dark:bg-default/60',
-              'backdrop-blur-xl', 'backdrop-saturate-200',
-              'hover:bg-default-200/70', 'dark:hover:bg-default/70',
-              'group-data-[focused=true]:bg-default-200/50',
-              'dark:group-data-[focused=true]:bg-default/60', '!cursor-text'
-            ]
-          }}
-          placeholder='Type to search...'
-          startContent={
-            <LuSearch className='text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0' />
-          }
-        />
+        {/* Votre code pour la barre de recherche */}
       </div>
       <br />
       <div className='cards flex gap-4'>
-        {jobOffers && jobOffers.map((jobOffer, index) => (
+        {data.map((item, index) => (
           <Card key={index} className='max-w-[400px]'>
             <CardHeader className='flex gap-3'>
               <Image
@@ -87,29 +65,28 @@ export default function JobOffers () {
             </CardHeader>
             <Divider />
             <CardBody>
-              <h2 className='text-xl text-center my-8'>{jobOffer.attributes.title}</h2>
+              <h2 className='text-xl text-center my-8'>{item.attributes.title}</h2>
               <div className='description flex flex-col gap-3 justify-normal'>
                 <p><strong>Description:</strong></p>
                 <ScrollShadow className='w-[300px] h-[400px] overflow-auto'>
                   <div>
-                    <p><strong>Description:</strong></p>
-                    {jobOffer.attributes.description && jobOffer.attributes.description.map((paragraph, index) => (
-                      <p key={index}>{truncateDescription(paragraph.children.map(child => child.text))}</p>
+                    {item.attributes.description.map((paragraph, paragraphIndex) => (
+                      <p key={paragraphIndex}>{paragraph.children[0].text}</p>
                     ))}
                   </div>
                 </ScrollShadow>
                 <p><strong>Skills:</strong></p>
                 <div className='flex flex-wrap gap-4'>
-                  {jobOffer.attributes.skills.map((skill, index) => (
-                    <Tooltip key={index} content={skill}>
-                      <Button variant='flat' color={colors[index % colors.length]} className='capitalize'>
-                        {skill}
-                      </Button>
-                    </Tooltip>
+                  {item.attributes.skills.map((skill, skillIndex) => (
+                    <span key={skillIndex}>{skill}</span>
                   ))}
                 </div>
-                <p><strong>Education:</strong> {jobOffer.attributes.education.join(', ')}</p>
-                <p><strong>Start Date:</strong> {jobOffer.attributes.start_date}</p>
+                <p><strong>Education:</strong>
+                  {item.attributes.education.map((education, educationIndex) => (
+                    <span key={educationIndex}>{education}</span>
+                  ))}
+                </p>
+                <p><strong>Start Date:</strong> {item.attributes.start_date}</p>
               </div>
             </CardBody>
             <Divider />
