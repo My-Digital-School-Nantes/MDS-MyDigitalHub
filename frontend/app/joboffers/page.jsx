b5d73b2bd5b5c3fafbd498fdb02884e0b09fb427
Romain Garganigo <romain.garganigo@sensorfactory.eu>
2024-04-26 11:12:28 +0200

    #2 Searchbar + Scroll description + actions button + fetch all offers

--- frontend/app/joboffers/page.jsx
+++ frontend/app/joboffers/page.jsx
@@ -8,67 +8,99 @@ import {
   DropdownItem,
   Button,
   Tooltip
+  , Input
+  , ScrollShadow
 } from '@nextui-org/react'
 
+import { LuSearch } from 'react-icons/lu'
+
 export default function JobOffers () {
-  const [jobOffer, setJobOffer] = useState(null)
+  const [jobOffers, setJobOffers] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:1337/api/offers/1', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    fetch('http://localhost:1337/api/offers')
       .then(response => response.json())
       .then(data => {
         console.log('Data fetched:', data)
-        setJobOffer(data.data.attributes)
+        setJobOffers(data.data)
       })
-      .catch(error => console.error('Error fetching job offer:', error))
+      .catch(error => console.error('Error fetching job offers:', error))
   }, [])
 
   const colors = [
-    'default',
-    'primary',
-    'secondary',
-    'success',
-    'warning',
-    'danger',
-    'foreground'
+    'default', 'primary', 'secondary', 'success', 'warning', 'danger', 'foreground'
   ]
 
-  return (
+  function truncateDescription (description) {
+    const maxLength = 50
+    if (description.length > maxLength) {
+      return `${description.slice(0, maxLength)}...` // Tronquer la description si elle dépasse la longueur maximale
+    }
+    return description
+  }
 
+  return (
     <>
       <h1 className='text-4xl text-center my-8'>MDS Job Offers</h1>
-      <Card className='max-w-[400px]'>
-        <CardHeader className='flex gap-3'>
-          <Image
-            alt='mds logo'
-            height={40}
-            radius='sm'
-            src='http://localhost:1337/uploads/logomds_c5516472bc.webp'
-            width={40}
-          />
-          <div className='flex flex-col'>
-            <p className='text-md'>My Digital School</p>
-            <p className='text-small text-default-500'>mydigitalschool.com</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          {jobOffer && (
-            <>
-              <h2 className='text-xl text-center my-8'>{jobOffer.title}</h2>
-              <div className='description flex flex-col gap-3'>
+      <div>
+        <Input
+          label='Search'
+          isClearable
+          radius='lg'
+          classNames={{
+            label: 'text-black/50 dark:text-white/90',
+            input: [
+              'bg-transparent', 'text-black/90 dark:text-white/90',
+              'placeholder:text-default-700/50 dark:placeholder:text-white/60'
+            ],
+            innerWrapper: 'bg-transparent',
+            inputWrapper: [
+              'shadow-xl', 'bg-default-200/50', 'dark:bg-default/60',
+              'backdrop-blur-xl', 'backdrop-saturate-200',
+              'hover:bg-default-200/70', 'dark:hover:bg-default/70',
+              'group-data-[focused=true]:bg-default-200/50',
+              'dark:group-data-[focused=true]:bg-default/60', '!cursor-text'
+            ]
+          }}
+          placeholder='Type to search...'
+          startContent={
+            <LuSearch className='text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0' />
+          }
+        />
+      </div>
+      <br />
+      <div className='cards flex gap-4'>
+        {jobOffers && jobOffers.map((jobOffer, index) => (
+          <Card key={index} className='max-w-[400px]'>
+            <CardHeader className='flex gap-3'>
+              <Image
+                alt='mds logo'
+                height={40}
+                radius='sm'
+                src='http://localhost:1337/uploads/logomds_c5516472bc.webp'
+                width={40}
+              />
+              <div className='flex flex-col'>
+                <p className='text-md'>My Digital School</p>
+                <p className='text-small text-default-500'>mydigitalschool.com</p>
+              </div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <h2 className='text-xl text-center my-8'>{jobOffer.attributes.title}</h2>
+              <div className='description flex flex-col gap-3 justify-normal'>
                 <p><strong>Description:</strong></p>
-                {jobOffer.description && jobOffer.description.map((paragraph, index) => (
-                  <p key={index}>{paragraph.children.map(child => child.text)}</p>
-                ))}
+                <ScrollShadow className='w-[300px] h-[400px] overflow-auto'>
+                  <div>
+                    <p><strong>Description:</strong></p>
+                    {jobOffer.attributes.description && jobOffer.attributes.description.map((paragraph, index) => (
+                      <p key={index}>{truncateDescription(paragraph.children.map(child => child.text))}</p>
+                    ))}
+                  </div>
+                </ScrollShadow>
                 <p><strong>Skills:</strong></p>
                 <div className='flex flex-wrap gap-4'>
-                  {jobOffer.skills.map((skill, index) => (
+                  {jobOffer.attributes.skills.map((skill, index) => (
                     <Tooltip key={index} content={skill}>
                       <Button variant='flat' color={colors[index % colors.length]} className='capitalize'>
                         {skill}
@@ -76,34 +108,29 @@ export default function JobOffers () {
                     </Tooltip>
                   ))}
                 </div>
-
-                <p><strong>Education:</strong> {jobOffer.education.join(', ')}</p>
-                <p><strong>Start Date:</strong> {jobOffer.start_date}</p>
+                <p><strong>Education:</strong> {jobOffer.attributes.education.join(', ')}</p>
+                <p><strong>Start Date:</strong> {jobOffer.attributes.start_date}</p>
               </div>
-            </>
-          )}
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button
-                variant='bordered'
-              >
-                Actions
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label='Static Actions'>
-              <DropdownItem key='new'>Apply</DropdownItem>
-              <DropdownItem key='copy'>Add to favorite</DropdownItem>
-              <DropdownItem key='edit'>Share offer</DropdownItem>
-              <DropdownItem key='delete' className='text-danger' color='danger'>
-                Hide offer
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </CardFooter>
-      </Card>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button variant='bordered'>Actions</Button>
+                </DropdownTrigger>
+                <DropdownMenu aria-label='Static Actions'>
+                  <DropdownItem key='new'>Apply</DropdownItem>
+                  <DropdownItem key='copy'>Add to favorite</DropdownItem>
+                  <DropdownItem key='edit'>Share offer</DropdownItem>
+                  <DropdownItem key='delete' className='text-danger' color='danger'>
+                    Hide offer
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   )
 }
