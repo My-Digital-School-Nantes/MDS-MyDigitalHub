6d6af074f2269a90d13d0c3d2c46293e7c47aef2
Phaerya <joaquim.morais-faria@outlook.com>
2024-05-16 11:15:15 +0200

    2 - Create offer through modal

--- frontend/components/jobOfferAdd/addJobOfferModal.jsx
+++ frontend/components/jobOfferAdd/addJobOfferModal.jsx
@@ -1,9 +1,43 @@
 'use client'
-import React from 'react'
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react'
+import React, { useState } from 'react'
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input } from '@nextui-org/react'
+import { CREATE_OFFER } from '@/graphql/queries/queries'
+import client from '@/graphql/apolloClient'
 
 export default function AddJobOfferModal () {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure()
+
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    skills: '',
+    education: '',
+    start_date: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const result = await client.mutate({
+        mutation: CREATE_OFFER,
+        variables: {
+          data: formData
+        }
+      })
+      console.log('result FORMDATA titre: ', formData.title)
+      console.log('Offer created:', result.data.createOffer)
+      onClose()
+    } catch (error) {
+      console.error('Error creating offer:', error)
+    }
+  }
 
   return (
     <>
@@ -34,38 +68,22 @@ export default function AddJobOfferModal () {
         }}
       >
         <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className='flex flex-col gap-1'>Modal Title</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis.
-                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
-                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
-                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color='danger' variant='light' onPress={onClose}>
-                  Close
-                </Button>
-                <Button color='primary' onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
+          <ModalHeader className='flex flex-col gap-1'>Ajouter une offre d'emploi</ModalHeader>
+          <ModalBody>
+            <Input name='title' placeholder='Titre' value={formData.title} onChange={handleChange} />
+            <Input name='description' placeholder='Description' value={formData.description} onChange={handleChange} />
+            <Input name='skills' placeholder='Compétences' value={formData.skills} onChange={handleChange} />
+            <Input name='education' placeholder='Éducation' value={formData.education} onChange={handleChange} />
+            <Input name='start_date' placeholder='Date de début' type='date' value={formData.start_date} onChange={handleChange} />
+          </ModalBody>
+          <ModalFooter>
+            <Button color='danger' variant='light' onPress={onClose}>
+              Fermer
+            </Button>
+            <Button color='primary' onPress={handleSubmit}>
+              Ajouter
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
