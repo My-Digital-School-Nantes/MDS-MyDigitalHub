59c72aaf6d58a654075f5566ae0f76f2ca52593d
LaChancee <miharisoababef@gmail.com>
2024-05-24 23:44:06 +0200

    chore: Refactor project page component and add NextUI components #1

--- frontend/app/projets/[slug]/page.jsx
+++ frontend/app/projets/[slug]/page.jsx
@@ -1,4 +1,7 @@
+import { Card, CardFooter } from '@nextui-org/card'
 import { getProject } from './getProjectAction'
+import { Chip, Image, User } from '@nextui-org/react'
+import Markdown from 'react-markdown'
 
 export default async function Projets (props) {
   const { slug } = props.params
@@ -7,22 +10,37 @@ export default async function Projets (props) {
   console.log(project[0])
   console.log(slug)
   return (
-    <div className='max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24 '>
-
-      <div className='relative'>
-        <img
-          src={'http://localhost:1337/' + projet?.image?.data?.attributes?.url}
-          alt={projet?.image?.data?.attributes?.alt}
-          className='w-full h-64 object-cover rounded-t-lg'
-        />
-        <div className='absolute top-0 left-0 bg-gradient-to-r from-primary to-blue-400 text-white p-2 rounded-bl-lg'>
-          {projet.title}
+    <div className='grid gap-6 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24 '>
+      <div className='text-center'>
+        <h1 className='text-4xl sm:text-6xl font-bold text-gray-800 dark:text-gray-200'>
+          <span className='bg-gradient-to-r from-primary to-blue-400 text-transparent bg-clip-text '> {projet.title} </span>
+        </h1>
+      </div>
+      <Card>
+        <div className='flex justify-center relative'>
+          <Image
+            isBlurred
+            src={'http://localhost:1337' + (projet.image && projet.image.data && projet.image.data[0].attributes.url)}
+            fallbackSrc='https://via.placeholder.com/300x100'
+            alt='NextUI Album Cover'
+            className='p-5'
+          />
         </div>
-      </div>
-      <div className='p-4'>
-        <h2 className='text-xl font-bold text-gray-800 dark:text-gray-200'>{projet.title}</h2>
-        <p className='text-gray-600 dark:text-gray-400'>{projet.description}</p>
-      </div>
+        <div className=' grid gap-4 p-4'>
+          <Chip radius='sm' color='primary'>{projet.category}</Chip>
+          <Markdown className='text-left'>{projet.description}</Markdown>
+        </div>
+        <CardFooter className='flex justify-between items-end'>
+          <User
+            name={projet.creator}
+            description={projet.category}
+            avatarProps={{
+              src: 'http://localhost:1337' + projet?.creatorPhoto?.data?.attributes?.url
+            }}
+          />
+        </CardFooter>
+      </Card>
+
     </div>
   )
 }
